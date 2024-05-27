b5e032f6829c37d25437e566c1b6478e42b9c0d8
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2024-05-27 16:53:54 +0700

    first commit

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject 
+{
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed = 25f;
+    [SerializeField] List<GameObject> enemyPrefabs;
+    [SerializeField] float timeBetweenEnemySpawns = 1f;
+    [SerializeField] float spawmTimeVariance = 0f;
+    [SerializeField] float miniumSpawnTime = 0.2f;
+
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
+    public  Transform GetStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> waypoints = new List<Transform>();
+        foreach(Transform child in pathPrefab)
+        {
+           waypoints.Add(child); 
+        }
+        return waypoints;
+    }
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+    public float GetRandomSpawnTime()
+    {
+        float spawnTime = Random.Range(timeBetweenEnemySpawns - spawmTimeVariance,
+                                        timeBetweenEnemySpawns + spawmTimeVariance);
+        return Mathf.Clamp(spawnTime, miniumSpawnTime, float.MaxValue);
+    }
+}
